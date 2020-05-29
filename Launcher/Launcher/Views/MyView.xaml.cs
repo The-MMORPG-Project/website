40451c0781ae03e87612bd2b8f3b828c28fc3191
valkyrienyanko <sebastianbelle074@gmail.com>
2020-05-29 16:23:11 -0500

    Update

---
+++ Launcher/Launcher/Views/MyView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Launcher.Views
+{
+    public class MyView : UserControl
+    {
+        public MyView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
