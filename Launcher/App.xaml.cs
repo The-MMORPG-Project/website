dd6c9b1a689368c437405329c0fd36328487c2bf
valkyrienyanko <sebastianbelle074@gmail.com>
2020-05-29 13:10:47 -0500

    Update

---
+++ Launcher/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Launcher.ViewModels;
+using Launcher.Views;
+
+namespace Launcher
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
