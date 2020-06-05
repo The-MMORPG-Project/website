321d119007c322d8ba8e547ca7105cca87669b87
valkyrienyanko <sebastianbelle074@gmail.com>
2020-06-05 14:41:05 -0500

    Migrate to Arrow Functions

--- Launcher/src/script/utils.js
+++ Launcher/src/script/utils.js
@@ -1,11 +1,3 @@
-function id(id) {
-    return document.getElementById(id)
-}
-
-function round(n, d) {
-    return Math.round(n * (10 ** d)) / (10 ** d)
-}
-
-function lerp(v0, v1, t) {
-	return (1 - t) * v0 + t * v1
-}
+const id = (id) => document.getElementById(id)
+const round = (n, d) => Math.round(n * (10 ** d)) / (10 ** d)
+const lerp = (v0, v1, t) => (1 - t) * v0 + t * v1
