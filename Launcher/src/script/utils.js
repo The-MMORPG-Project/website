aff3a84a8be53b6b2c78125c10b72282f16cfb62
valkyrienyanko <sebastianbelle074@gmail.com>
2020-06-05 14:37:39 -0500

    Rename Args

--- Launcher/src/script/utils.js
+++ Launcher/src/script/utils.js
@@ -6,6 +6,6 @@ function round(n, d) {
     return Math.round(n * (10 ** d)) / (10 ** d)
 }
 
-function lerp(start, end, amt) {
-	return (1 - amt) * start + amt * end
+function lerp(v0, v1, t) {
+	return (1 - t) * v0 + t * v1
 }
