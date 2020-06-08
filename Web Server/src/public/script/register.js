1e65f1d49ace2bd5b97edf88e1e441af1d95ceb2
valkyrienyanko <sebastianbelle074@gmail.com>
2020-06-08 18:27:52 -0500

    Register Logic

--- Web Server/src/public/script/register.js
+++ Web Server/src/public/script/register.js
@@ -1,17 +1,89 @@
 const elements = {
+    email: id('email'),
     username: id('username'),
     password: id('password'),
+    confirmPassword: id('confirmPassword'),
     submit: id('submit'),
+    title: id('title'),
+    subtitle: id('subtitle'),
+    emailDiv: id('email-div'),
     message: id('message')
 }
 
-elements.submit.addEventListener('click', () => {
-    const username = elements.username.value
-    const password = elements.password.value
+const formElements = [email, username, password, confirmPassword]
+
+
+const resetClasses = () => {
+    formElements.forEach(element => {
+        if (element.classList.contains("invalid")) {
+            element.classList.remove("invalid")
+        }
+    })
+}
+
+const validateForm = () => {
+    removeMessage('notifyEmail')
+    removeMessage('notifyUsername')
+    removeMessage('notifyPassword')
+    removeMessage('notifyPasswordConfirm')
+
+    let emailValid = true
+    let usernameValid = true
+    let passwordValid = true
+    let passwordConfirmValid = true
+
+    // Validate email
+    const email = elements.email
+    if (!/^\S+@\S+$/.test(email.value) || email.value === "") {
+        createMessage('notifyEmail', 'emailContainer', 'Please provide a valid email', 'The email is invalid')
+        email.classList.add("invalid")
+
+    } else if (email.value.length < 6 || email.value.length > 50) {
+        createMessage('notifyEmail', 'emailContainer', 'Please provide a valid email', 'Must be between 6 and 50 characters')
+        email.classList.add("invalid")
+        emailValid = false
+    }
+
+    // Validate username
+    const username = elements.username
+    if (!/[a-zA-Z0-9_]/.test(username.value) || username.value === "") {
+        createMessage('notifyUsername', 'usernameContainer', 'Please provide a valid username', 'Must contain only alphanumeric characters')
+        username.classList.add("invalid")
+    } else if (username.value.length < 3 || username.value.length > 15) {
+        createMessage('notifyUsername', 'usernameContainer', 'Please provide a valid username', 'Must be between 3 to 15 characters')
+        username.classList.add("invalid")
+        usernameValid = false
+    }
+
+    // Validate password
+    const password = elements.password
+    if (password.value === "" || password.value.length < 5 || password.value.length > 30) {
+        createMessage('notifyPassword', 'passwordContainer', 'Please provide a valid password', 'Must be between 5 to 30 characters')
+        password.classList.add("invalid")
+        passwordValid = false
+    }
+
+    // Validate confirm password
+    const confirmPassword = elements.confirmPassword
+    if (password.value !== confirmPassword.value || confirmPassword.value === '') {
+        createMessage('notifyPasswordConfirm', 'confirmPasswordContainer', 'Please provide a valid password', 'Passwords do not match')
+        confirmPassword.classList.add("invalid")
+        passwordConfirmValid = false
+    }
+
+    if (!emailValid || !usernameValid || !passwordValid || !passwordConfirmValid) {
+        return false
+    }
+
+    return true
+}
+
+const sendForm = () => {
+    console.log("Sending register request to server...")
 
     axios.post('/api/register', {
-        username,
-        password
+        username: elements.username.value,
+        password: elements.password.value
     }).then((response) => {
         const data = response.data
 
@@ -36,8 +108,49 @@ elements.submit.addEventListener('click', () => {
     }).catch((error) => {
         console.log(error)
     })
+}
+
+elements.submit.addEventListener('click', () => {
+    resetClasses()
+    setTimeout(() => {
+        if (!validateForm()) {
+            return
+        }
+
+        sendForm()
+    }, 1)
 })
 
-function updateMessage(message) {
-    elements.message.innerHTML = message
+function createMessage(id, appendID, titleText, subtitleText) {
+    const div = document.createElement('div')
+    const title = document.createElement('p')
+    const subtitle = document.createElement('p')
+
+    div.id = id
+    div.className = 'message'
+
+    div.appendChild(title)
+    div.appendChild(subtitle)
+
+    title.className = "title"
+    subtitle.className = "subtitle"
+
+    title.innerText = titleText
+    subtitle.innerHTML = subtitleText
+
+    const container = document.getElementById(appendID)
+    container.appendChild(div)
+}
+
+function removeMessage(id) {
+    const element = document.getElementById(id)
+    if (element != null) {
+        element.parentNode.removeChild(element)
+    }
+}
+
+function updateMessage(text) {
+    const element = document.getElementById('serverMessage')
+    element.style.visibility = 'visible'
+    element.innerHTML = text
 }
